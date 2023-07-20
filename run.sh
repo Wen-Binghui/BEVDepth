ff415ace3ebbbcc9902f322a81dac9aee72aec60
Haoang Li <lhao@atcremers95.cvai.in.tum.de>
2023-07-20 21:54:29 +0200

    accelerator: ddp => cuda

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python /mnt/data/codes/BEVDepth/bevdepth/exps/nuscenes/fusion/bev_depth_fusion_lss_r50_256x704_128x128_24e_2key_trainval.py --amp_backend native -b 8 --gpus 1
+python bevdepth/exps/nuscenes/fusion/bev_depth_fusion_lss_r50_256x704_128x128_24e_2key_trainval.py --amp_backend native -b 4 --gpus 1
